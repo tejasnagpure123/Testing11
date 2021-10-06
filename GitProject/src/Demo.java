e129e1b4f5c1b2f5467ff9d2cb82cf9a8c5e61c9
tejasnagpure123 <92013232+tejasnagpure123@users.noreply.github.com>
2021-10-06 15:37:49 +0530

    Tejas

--- GitProject/src/Demo.java
+++ GitProject/src/Demo.java
@@ -9,6 +9,10 @@ public class Demo {
 	{
 		System.out.println("This is m1 method");
 	}
+	public void m3()
+	{
+		System.out.println("This is m3 method");
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 	}
