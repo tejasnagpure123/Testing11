2bf2c41c67f10fe7ed772d971a2913a9f7fe7a8e
tejasnagpure123 <92013232+tejasnagpure123@users.noreply.github.com>
2021-10-08 12:19:37 +0530

    Add files via upload

---
+++ DemoGit2.java
@@ -0,0 +1,14 @@
+package com.cjc;
+
+public class DemoGit2 {
+
+	public void m2()
+	{
+		System.out.println("This is m2");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HEllo git");
+	}
+
+}
